f3b8fad8bac157f2ae17aab37c581eb7b5393a21
Adalbertus250 <killergames156@gmail.com>
2023-01-22 00:40:20 +0100

    Vytvořený test pro pint

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -19,9 +19,7 @@ abstract class DuskTestCase extends BaseTestCase
      */
     public static function prepare(): void
     {
-        if (env('CI_ENABLED', false)) {
-            return;
-        }
+        if (env('CI_ENABLED', false)) return;
 
         if (! static::runningInSail()) {
             static::startChromeDriver();
