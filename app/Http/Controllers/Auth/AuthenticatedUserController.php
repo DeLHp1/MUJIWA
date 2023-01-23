9f340f6f722b7e981c8fa8497acbdf844f2c5eec
Adalbertus250 <killergames156@gmail.com>
2023-01-23 20:02:19 +0100

    :necktie: Přidána logika do kontroleru

--- app/Http/Controllers/Auth/AuthenticatedUserController.php
+++ app/Http/Controllers/Auth/AuthenticatedUserController.php
@@ -3,9 +3,58 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\ValidationException;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class AuthenticatedUserController extends Controller
 {
-    //
+    /**
+     * Funkce pro render login stránky
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        return Inertia::render('auth/login');
+    }
+
+    /**
+     * Funkce starající se o přihlášení do aplikace
+     *
+     * @param LoginRequest $request
+     * @return RedirectResponse
+     * @throws ValidationException
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->AuthUser();
+
+        $request->session()->regenerate();
+
+        return Redirect::intended();
+    }
+
+    /**
+     * Funkce starající se o odhlášení uživatele z aplikace
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return Redirect::intended();
+    }
 }
