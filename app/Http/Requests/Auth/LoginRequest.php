888e0c9ceb827378cd8c238480ece70285403be2
Adalbertus250 <killergames156@gmail.com>
2023-01-23 19:51:22 +0100

    :necktie: Přidána logika pro přihlášení včetně automatické validace do loginRequestu

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Requests\Auth;
 
+use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
@@ -13,7 +18,7 @@ class LoginRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +29,64 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'username' => ['required', 'string'],
+            'password' => ['required', 'string'],
         ];
+    }
+
+    /**
+     * Funkce starající se o authentifikaci uživatele
+     *
+     * @return void
+     * @throws ValidationException
+     */
+    public function AuthUser(): void
+    {
+        $this->ensureIsNotRateLimited();
+
+        if (! Auth::attempt($this->only('username', 'password'), $this->boolean('remember'))){
+            RateLimiter::hit($this->throttleKey());
+
+            throw ValidationException::withMessages([
+               'username' => trans('auth.failed'),
+            ]);
+        }
+
+        RateLimiter::clear($this->throttleKey());
+    }
+
+    /**
+     * Funkce starající se o kontrolu zda uživatel už nepřesáhl mnoho pokusu
+     * a v případě přesáhnutí informovat uživatele na email o pokus se přihlásit.
+     *
+     * @return void
+     * @throws ValidationException
+     */
+    protected function ensureIsNotRateLimited(): void
+    {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            return;
+        }
+
+        event(new Lockout($this));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'email' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+    }
+
+    /**
+     * Funkce vracející klíč pro RateLimiter
+     *
+     * @return string
+     */
+    protected function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->input('username').'|'.$this->ip().'|'.$this->fingerprint()));
     }
 }
