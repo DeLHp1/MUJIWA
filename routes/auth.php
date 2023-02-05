94b1ee1d1c186ff862f453b36b3d2e73117d6f41
Adalbertus250 <killergames156@gmail.com>
2023-02-05 03:43:27 +0100

    Přidán router pro register

--- routes/auth.php
+++ routes/auth.php
@@ -24,6 +24,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['guest'])->group(function (){
     Route::get('/login', [\App\Http\Controllers\Auth\AuthenticatedUserController::class, 'index'])->name('login');
     Route::post('/login', [\App\Http\Controllers\Auth\AuthenticatedUserController::class, 'store']);
+    Route::get('/register', [\App\Http\Controllers\Auth\RegisteredUserController::class, 'index'])->name('register');
+    Route::post('/register', [\App\Http\Controllers\Auth\RegisteredUserController::class, 'store']);
 });
 
 Route::middleware(['auth:web'])->group(function (){
