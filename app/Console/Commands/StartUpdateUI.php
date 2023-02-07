1fb3413cc8287c1b7cd3274ba4413b819b4000ce
Adalbertus250 <killergames156@gmail.com>
2023-02-07 09:56:13 +0100

    Vytvořeny příkazy pro spuštění Aktualizace

---
+++ app/Console/Commands/StartUpdateUI.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\UpdateUIStart;
+use Illuminate\Console\Command;
+
+class StartUpdateUI extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'updater:start {title="Vyšla nová verze MW UI"} {message="MW UI V3 se začne automaticky aktualizovat."}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Funkce pro spuštění aktualizace MW UI V3';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $title = $this->argument('title');
+        $message = $this->argument('message');
+
+        event(new UpdateUIStart($title, $message));
+
+        return Command::SUCCESS;
+    }
+}
