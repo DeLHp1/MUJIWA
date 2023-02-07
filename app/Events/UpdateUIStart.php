f71800464ce5212c0b74bbd061ce5d18aa029ac7
Adalbertus250 <killergames156@gmail.com>
2023-02-07 09:56:52 +0100

    Vytvořeny jednotlivý eventy pro real-time update

---
+++ app/Events/UpdateUIStart.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateUIStart implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public string $title;
+
+    public string $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($title, $message)
+    {
+        $this->title = $title;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('mw-ui-updater');
+    }
+
+    public function broadcastAs()
+    {
+        return 'mw-ui-updater.start';
+    }
+}
